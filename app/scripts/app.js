56459cc3fc96a50d5348dde653c82f8531aef174
funhead <michaelishmael1976@gmail.com>
2014-07-01 17:43:45 +0100

    Added scaffolding for Angular app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gnRsiteApp
+ * @description
+ * # gnRsiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gnRsiteApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gnRsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gnRsiteApp
+ */
+angular.module('gnRsiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
